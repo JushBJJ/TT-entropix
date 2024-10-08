f361eecd93acb3eecab4ed22918415b22be6d29e
JushBJJ <JushPacis@gmail.com>
2024-10-08 13:10:49 +0000

    Switch to ttnn.rms_norm for RMSnorm

--- entropix/ttnn/ttnn_model.py
+++ entropix/ttnn/ttnn_model.py
@@ -3,8 +3,8 @@ import torch
 
 from entropix.ttnn.ttnn_weights import TTNNLayerWeights
 
-def ttnn_rms_norm(x: ttnn.Tensor, w: ttnn.Tensor, eps: float=1e-6) -> ttnn.Tensor:
-    return w * (x * ttnn.rsqrt(ttnn.mean(ttnn.pow(x, 2), dim=-1, keepdim=True) + eps))
+def ttnn_rms_norm(x: ttnn.Tensor, w: ttnn.Tensor) -> ttnn.Tensor:
+    return ttnn.rms_norm(input_tensor=x, weight=w)
 
 def ttnn_feedforward(x: ttnn.Tensor, layer_weights: TTNNLayerWeights) -> ttnn.Tensor:
     return ttnn.linear(ttnn.silu(ttnn.linear(x, layer_weights.w1)) * ttnn.linear(x, layer_weights.w3), layer_weights.w2)
